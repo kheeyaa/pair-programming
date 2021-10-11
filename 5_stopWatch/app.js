0ec11d671e74a748d7e7faea0e3ce0b8d56c06e6
kheeyaa <kheeyaa@icloud.com>
2021-10-11 20:06:15 +0900

    feat: Update StopWatch JS
    10ms 단위로 시간을 증가시켜, ms, s, m 단위로 시간을 출력하는 함수를 구현함.

--- 5_stopWatch/app.js
+++ 5_stopWatch/app.js
@@ -5,23 +5,34 @@ const $leftControl = $stopwatch.children[1];
 const $rightControl = $stopwatch.children[2];
 
 let isRunning = false;
-
 let interval = 0;
 
-const runTimer = () => {
-  setInterval(() => {
-    interval += 10;
-    if (!isRunning) {
-      clearInterval(runTimer);
-    }
-  }, 10);
+const formatTime = () => {
+  const millisecond = interval % 100;
+  const second = Math.floor(interval / 100) % 60;
+  const minute = Math.floor(interval / 6000);
+
+  return `${minute > 9 ? minute : '0' + minute}:${
+    second > 9 ? second : '0' + second
+  }:${millisecond > 9 ? millisecond : '0' + millisecond}`;
+};
+
+const render = () => {
+  $display.innerHTML = formatTime();
 };
 
 const startTimer = () => {
   $rightControl.removeAttribute('disabled');
   $leftControl.innerHTML = 'Stop';
   $rightControl.innerHTML = 'Lap';
-  runTimer();
+
+  const runTimer = setInterval(() => {
+    interval += 1;
+    if (!isRunning) {
+      clearInterval(runTimer);
+    }
+    render();
+  }, 10);
 };
 
 const stopTimer = () => {
