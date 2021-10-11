8b128ab42a4424613a4dd46b9a2ae47f36172c64
kheeyaa <kheeyaa@icloud.com>
2021-10-11 16:28:08 +0900

    feat: Update ScrollingGotoTop JS
    스로틀을 이용해서 짧은 시간안에 이벤트가 과도하게 발생하여 이벤트 핸들러가 호출되는 현상을 방지하였다.

--- 2_scrollingGotoTop/app.js
+++ 2_scrollingGotoTop/app.js
@@ -1,12 +1,27 @@
 // DOM Nodes
 const $scrollIcon = document.querySelector('.scroll-icon');
 
+const throttle = (callback, delay) => {
+  let timerId;
+  return event => {
+    if (timerId) return;
+    timerId = setTimeout(
+      () => {
+        callback(event);
+        timerId = null;
+      },
+      delay,
+      event
+    );
+  };
+};
+
 // Event Binding
 
-window.onscroll = () => {
-  console.log('A');
+window.onscroll = throttle(() => {
+  console.log(window.pageYOffset);
   $scrollIcon.style.display = window.pageYOffset > 100 ? 'block' : 'none';
-};
+}, 300);
 
 $scrollIcon.onclick = () => {
   window.scrollTo(0, 0);
