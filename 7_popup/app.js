fc9ba8a9a9b017820df495748a90d539f38c75e7
kheeyaa <kheeyaa@icloud.com>
2021-10-12 13:41:29 +0900

    feat: Add Modal HTML, CSS, JS
    마크업과 스타일링 JS까지 완료

---
+++ 7_popup/app.js
@@ -0,0 +1,26 @@
+const $togglePopup = document.querySelector('.toggle-popup');
+const $modalWrap = document.querySelector('.modal-wrap');
+const $modalCancel = document.querySelector('.modal-cancel');
+const $modalClose = document.querySelector('.modal-close');
+const $modalInput = document.querySelector('.modal-input');
+const $modalForm = document.querySelector('.modal-form');
+const $popupMessage = document.querySelector('.popup-message');
+
+$togglePopup.onclick = () => {
+  $modalInput.value = '';
+  $modalWrap.classList.remove('hidden');
+  $modalInput.focus();
+};
+
+$modalCancel.onclick = () => {
+  $modalWrap.classList.add('hidden');
+};
+$modalClose.onclick = () => {
+  $modalWrap.classList.add('hidden');
+};
+
+$modalForm.onsubmit = e => {
+  e.preventDefault();
+  $popupMessage.innerHTML = $modalInput.value;
+  $modalWrap.classList.add('hidden');
+};
