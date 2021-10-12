d16f387a574f9f4bf7c0efbc7a479b19413dd41c
kheeyaa <kheeyaa@icloud.com>
2021-10-12 13:03:25 +0900

    feat: Update DarkMode JS
    사용자 운영체제의 dark모드를 기본값으로 지정

--- 6_darkMode/darkmode.js
+++ 6_darkMode/darkmode.js
@@ -2,13 +2,13 @@ const $body = document.querySelector('body');
 
 let isDarkMode = false;
 
+const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
 const fetchState = () => {
   isDarkMode = localStorage.getItem('isDarkMode')
     ? JSON.parse(localStorage.getItem('isDarkMode'))
-    : false;
+    : darkModeMediaQuery.matches;
 };
-
-// window.matchMedia('(prefers-color-scheme: dark)')
 
 fetchState();
 $body.classList.toggle('dark', isDarkMode);
