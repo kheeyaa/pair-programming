22879242aaba374e6ba11402ae2048abba27c66c
skojphy <skojphy@gmail.com>
2021-10-12 19:26:56 +0900

    feat: Modify Accordion JS
    아코디언 메뉴를 클릭할 경우 다른 아코디언이 닫히는 효과를 구현함.

--- 9_accordion/app.js
+++ 9_accordion/app.js
@@ -8,7 +8,9 @@ const activeAccordion = target => {
   });
 };
 
-const showMenu = target => {
+const getHeight = target => {
+  //   if (!target.parentNode.classList.contains('active')) return;
+
   const totalHeight = [...target.children].reduce(
     (height, subMenu) =>
       height +
@@ -16,11 +18,27 @@ const showMenu = target => {
         1,
     0
   );
-  target.style.height = totalHeight + 'px';
+  return totalHeight + 'px';
 };
+
+const showMenu = target => {
+  [...$accordion.children].forEach(menuContainer => {
+    menuContainer.lastElementChild.style.height =
+      menuContainer.classList.contains('active') ? getHeight(target) : 0;
+  });
+};
+
+const initialMenu = () => {
+  const $active = document.querySelector('.active');
+  $active.lastElementChild.style.height = 'auto';
+};
+
+// initialMenu에서 초기 height를 auto로 한 결과 뚝딱거림. 디버깅 필요함.
 
 $accordion.onclick = e => {
   if (!e.target.classList.contains('menu')) return;
   activeAccordion(e.target.parentNode);
   showMenu(e.target.nextElementSibling);
 };
+
+window.addEventListener('DOMContentLoaded', initialMenu);
