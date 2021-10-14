80263524c54cdaf6978d29959a9405be6d90fd29
kheeyaa <kheeyaa@icloud.com>
2021-10-14 11:51:10 +0900

    feat: Update form validation JS
    입력 유효성을 확인하여 로그인 버튼 활성화 까지 구현함.

--- 13_formValidation/js/auth.js
+++ 13_formValidation/js/auth.js
@@ -2,34 +2,74 @@
 
 const $signinUserid = document.getElementById('signin-userid');
 const $signinPassword = document.getElementById('signin-password');
+const $signinBtn = document.querySelector('.signin.button');
 
 // varaibles
+// constants
 const ID_REGEXP =
-  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-const PW_REGEXP = /^[0-9a-zA-Z]{6,12}$/g;
+  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+const PW_REGEXP = /^[0-9a-zA-Z]{6,12}$/;
 const ID_ERROR_MESSAGE = '이메일 형식에 맞게 입력해 주세요.';
 const PW_ERROR_MESSAGE = '영문 또는 숫자를 6~12자 입력해 주세요.';
 
+// states
+let idValidateState = false;
+let pwValidateState = false;
+
 // functions
 
-const isValidate = (inputVal, regexp) => regexp.test(inputVal);
+const isValidate = (inputType, inputVal, regexp) => {
+  const validation = regexp.test(inputVal);
+  inputType === 'id'
+    ? (idValidateState = validation)
+    : (pwValidateState = validation);
+  return validation;
+};
 
-const renderError = (target, regexp, message) => {
-  target.parentNode.lastElementChild.innerHTML = isValidate(
-    target.value,
-    regexp
-  )
-    ? ''
-    : message;
-  console.log(isValidate(target.value, regexp));
+const noticeValidation = (inputType, target) => {
+  if (inputType !== 'id' && inputType !== 'pw')
+    throw new TypeError('input type Error!');
+
+  const [regexp, message] =
+    inputType === 'id'
+      ? [ID_REGEXP, ID_ERROR_MESSAGE]
+      : [PW_REGEXP, PW_ERROR_MESSAGE];
+
+  if (isValidate(inputType, target.value, regexp)) {
+    target.parentNode.lastElementChild.innerHTML = '';
+    [...target.parentNode.children].forEach(node =>
+      node.matches('.icon-success')
+        ? node.classList.remove('hidden')
+        : node.matches('.icon-error')
+        ? node.classList.add('hidden')
+        : node
+    );
+  } else {
+    target.parentNode.lastElementChild.innerHTML = message;
+    [...target.parentNode.children].forEach(node =>
+      node.matches('.icon-error')
+        ? node.classList.remove('hidden')
+        : node.matches('.icon-success')
+        ? node.classList.add('hidden')
+        : node
+    );
+  }
+};
+
+const activeSigninBtn = () => {
+  if (idValidateState && pwValidateState)
+    $signinBtn.removeAttribute('disabled');
+  else $signinBtn.setAttribute('disabled', '');
 };
 
 // Event Bindings
 
 $signinUserid.oninput = e => {
-  renderError(e.target, ID_REGEXP, ID_ERROR_MESSAGE);
+  noticeValidation('id', e.target);
+  activeSigninBtn();
 };
 
 $signinPassword.oninput = e => {
-  renderError(e.target, PW_REGEXP, PW_ERROR_MESSAGE);
+  noticeValidation('pw', e.target);
+  activeSigninBtn();
 };
