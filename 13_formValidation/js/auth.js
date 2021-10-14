55ab2305e1139d10697f3396d3271466b0be7c8f
kheeyaa <kheeyaa@icloud.com>
2021-10-14 16:04:59 +0900

    feat: Update Form validation JS
    회원가입창에서 비밀번호 확인 제외하고 구현함.

--- 13_formValidation/js/auth.js
+++ 13_formValidation/js/auth.js
@@ -7,37 +7,69 @@ const $signinPassword = document.getElementById('signin-password');
 const $signinBtn = document.querySelector('.signin.button');
 const $signinForm = document.querySelector('.signin.form');
 const $signupForm = document.querySelector('.signup.form');
+const $links = document.querySelectorAll('.link > a');
+const $signupUserid = document.getElementById('signup-userid');
+const $signupPassword = document.getElementById('signup-password');
+const $signupName = document.getElementById('signup-name');
+const $signupConfirmPassword = document.getElementById(
+  'signup-confirm-password'
+);
+
+const $signupBtn = document.querySelector('.signup.button');
 
 // varaibles
 // constants
 const ID_REGEXP =
   /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 const PW_REGEXP = /^[0-9a-zA-Z]{6,12}$/;
+const NAME_REGEXP = /^[A-Za-z가-힣]+$/;
+// const PW_CONFIRM_REGEXP = //
+
 const ID_ERROR_MESSAGE = '이메일 형식에 맞게 입력해 주세요.';
 const PW_ERROR_MESSAGE = '영문 또는 숫자를 6~12자 입력해 주세요.';
+const NAME_ERROR_MESSAGE = '이름을 입력해 주세요.';
+const PW_CONFIRM_ERROR_MESSAGE = '패스워드가 일치하지 않습니다.';
 
 // states
 let idValidateState = false;
 let pwValidateState = false;
+let nameValidateState = false;
+let confirmValidateState = false;
 
 // functions
+
+const isEqualPw = () => $signupPassword.value === $signupConfirmPassword.value;
 
 const isValidate = (inputType, inputVal, regexp) => {
   const validation = regexp.test(inputVal);
   inputType === 'id'
     ? (idValidateState = validation)
-    : (pwValidateState = validation);
+    : inputType === 'pw'
+    ? (pwValidateState = validation)
+    : inputType === 'name'
+    ? (nameValidateState = validation)
+    : (confirmValidateState = isEqualPw());
+
   return validation;
 };
 
 const noticeValidation = (inputType, target) => {
-  if (inputType !== 'id' && inputType !== 'pw')
+  if (
+    inputType !== 'id' &&
+    inputType !== 'pw' &&
+    inputType !== 'name' &&
+    inputType !== 'confirmPw'
+  )
     throw new TypeError('input type Error!');
 
   const [regexp, message] =
     inputType === 'id'
       ? [ID_REGEXP, ID_ERROR_MESSAGE]
-      : [PW_REGEXP, PW_ERROR_MESSAGE];
+      : inputType === 'pw'
+      ? [PW_REGEXP, PW_ERROR_MESSAGE]
+      : inputType === 'name'
+      ? [NAME_REGEXP, NAME_ERROR_MESSAGE]
+      : [PW_REGEXP, PW_CONFIRM_ERROR_MESSAGE];
 
   if (isValidate(inputType, target.value, regexp)) {
     target.parentNode.lastElementChild.innerHTML = '';
@@ -66,6 +98,17 @@ const activeSigninBtn = () => {
   else $signinBtn.setAttribute('disabled', '');
 };
 
+const activeSignupBtn = () => {
+  if (
+    idValidateState &&
+    pwValidateState &&
+    nameValidateState &&
+    confirmValidateState
+  )
+    $signupBtn.removeAttribute('disabled');
+  else $signupBtn.setAttribute('disabled', '');
+};
+
 // Event Bindings
 
 $signinUserid.oninput = e => {
@@ -78,7 +121,49 @@ $signinPassword.oninput = e => {
   activeSigninBtn();
 };
 
+// ---
+$signupUserid.oninput = e => {
+  noticeValidation('id', e.target);
+  activeSignupBtn();
+};
+
+$signupPassword.oninput = e => {
+  noticeValidation('pw', e.target);
+  activeSignupBtn();
+};
+
+$signupName.oninput = e => {
+  noticeValidation('name', e.target);
+  activeSignupBtn();
+};
+
+$signupConfirmPassword.oninput = e => {
+  noticeValidation('confirmPw', e.target);
+  activeSignupBtn();
+};
+// ---
+
 $signinForm.onsubmit = e => {
   e.preventDefault();
   toaster(e);
 };
+
+$signupForm.onsubmit = e => {
+  e.preventDefault();
+  toaster(e);
+};
+
+$links.forEach($el => {
+  $el.onclick = () => {
+    [$signupForm, $signinForm].forEach($form => {
+      $form.classList.toggle('hidden');
+    });
+
+    [
+      idValidateState,
+      pwValidateState,
+      nameValidateState,
+      confirmValidateState
+    ] = [false, false, false, false];
+  };
+});
