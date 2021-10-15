44ce14f6227cfd6100502954f0fb6be396f405e6
kheeyaa <kheeyaa@icloud.com>
2021-10-15 15:36:08 +0900

    refact: Refactor anolog clock
    renderClock 함수를 만들어서 분리함.

--- 4_analogClock/app.js
+++ 4_analogClock/app.js
@@ -1,8 +1,10 @@
+// DOM Nodes
 const $hour = document.querySelector('.hand.hour');
 const $minute = document.querySelector('.hand.minute');
 const $second = document.querySelector('.hand.second');
 
-setInterval(() => {
+// functions
+const renderClock = () => {
   const now = new Date();
   const hour = now.getHours();
   const minute = now.getMinutes();
@@ -11,4 +13,9 @@ setInterval(() => {
   $second.style.setProperty('--deg', second * 6);
   $minute.style.setProperty('--deg', minute * 6 + second * 0.1);
   $hour.style.setProperty('--deg', hour * 30 + minute * 0.5);
-}, 1000);
+};
+
+setInterval(renderClock, 1000);
+
+// event bindings
+window.addEventListener('DOMContentLoaded', renderClock);
