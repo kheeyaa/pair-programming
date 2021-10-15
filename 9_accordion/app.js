7ade1bb1f1985927e94a7d49ba022e5dc5ac2e0f
skojphy <skojphy@gmail.com>
2021-10-15 14:52:16 +0900

    refactor: Refactor Accordion JS
    변수명을 의미가 좀 더 명확하도록 수정함.

--- 9_accordion/app.js
+++ 9_accordion/app.js
@@ -1,30 +1,26 @@
 const $accordion = document.querySelector('.accordion');
 
-const activeAccordion = target => {
-  [...$accordion.children].forEach(menuContainer => {
-    menuContainer === target
-      ? menuContainer.classList.add('active')
-      : menuContainer.classList.remove('active');
+const activeAccordionMenu = target => {
+  [...$accordion.children].forEach($menuContainer => {
+    $menuContainer === target
+      ? $menuContainer.classList.add('active')
+      : $menuContainer.classList.remove('active');
   });
 };
 
 const getHeight = target => {
-  //   if (!target.parentNode.classList.contains('active')) return;
-
   const totalHeight = [...target.children].reduce(
-    (height, subMenu) =>
-      height +
-      window.getComputedStyle(subMenu).getPropertyValue('height').slice(0, -2) *
-        1,
+    (height, $subMenuItem) =>
+      height + getComputedStyle($subMenuItem).height.slice(0, -2) * 1,
     0
   );
   return totalHeight + 'px';
 };
 
 const showMenu = target => {
-  [...$accordion.children].forEach(menuContainer => {
-    menuContainer.lastElementChild.style.height =
-      menuContainer.classList.contains('active') ? getHeight(target) : 0;
+  [...$accordion.children].forEach($menuContainer => {
+    $menuContainer.lastElementChild.style.height =
+      $menuContainer.classList.contains('active') ? getHeight(target) : 0;
   });
 };
 
@@ -37,12 +33,11 @@ const initialMenu = () => {
   ).height;
 };
 
-// initialMenu에서 초기 height를 auto로 한 결과 뚝딱거림. 디버깅 필요함.
-
+// event bindings
 $accordion.onclick = e => {
   if (!e.target.classList.contains('menu')) return;
 
-  activeAccordion(e.target.parentNode);
+  activeAccordionMenu(e.target.parentNode);
   showMenu(e.target.nextElementSibling);
 };
 
