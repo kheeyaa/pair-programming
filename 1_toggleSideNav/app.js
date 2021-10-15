fa041cef2050d8724678a6bb1bfbb1e6b01917ef
kheeyaa <kheeyaa@icloud.com>
2021-10-15 16:26:33 +0900

    debug: debug transition effect
    toggleTransitionState 함수를 추가하여 초기 렌더링 시 transition이 발생하는 이슈를 해결함.

--- 1_toggleSideNav/app.js
+++ 1_toggleSideNav/app.js
@@ -4,6 +4,7 @@ let isActive = false;
 // DOM
 const $toggle = document.querySelector('.toggle');
 const $nav = document.querySelector('nav');
+const $main = document.querySelector('main');
 
 // functions
 const fetchState = () => {
@@ -19,12 +20,20 @@ const toggleNavigatorState = () => {
   $nav.classList.toggle('active', isActive);
 };
 
+const toggleTransitionState = isDisable => {
+  $nav.classList.toggle('notransition', isDisable);
+  $main.classList.toggle('notransition', isDisable);
+  $toggle.classList.toggle('notransition', isDisable);
+};
+
 // event bindings
 window.addEventListener('DOMContentLoaded', () => {
+  toggleTransitionState(true);
   fetchState();
   $nav.classList.toggle('active', isActive);
 });
 
 $toggle.onclick = () => {
+  toggleTransitionState(false);
   toggleNavigatorState();
 };
