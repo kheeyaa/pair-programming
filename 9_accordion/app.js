2cf9cec6fbe7544e894846c4772e484a6e12c2cb
skojphy <skojphy@gmail.com>
2021-10-15 14:30:31 +0900

    debug: Debug Accordion JS
    처음 렌더링 시 트랜지션이 발생하지 않도록 함.

--- 9_accordion/app.js
+++ 9_accordion/app.js
@@ -1,10 +1,10 @@
 const $accordion = document.querySelector('.accordion');
 
 const activeAccordion = target => {
-  [...$accordion.children].forEach(menu => {
-    menu === target
-      ? menu.classList.add('active')
-      : menu.classList.remove('active');
+  [...$accordion.children].forEach(menuContainer => {
+    menuContainer === target
+      ? menuContainer.classList.add('active')
+      : menuContainer.classList.remove('active');
   });
 };
 
@@ -31,12 +31,17 @@ const showMenu = target => {
 const initialMenu = () => {
   const $active = document.querySelector('.active');
   $active.lastElementChild.style.height = 'auto';
+
+  $active.lastElementChild.style.height = getComputedStyle(
+    $active.lastElementChild
+  ).height;
 };
 
 // initialMenu에서 초기 height를 auto로 한 결과 뚝딱거림. 디버깅 필요함.
 
 $accordion.onclick = e => {
   if (!e.target.classList.contains('menu')) return;
+
   activeAccordion(e.target.parentNode);
   showMenu(e.target.nextElementSibling);
 };
