ae70a28f7f3d7fb7845f3e04322dad3b2cfc4864
skojphy <skojphy@gmail.com>
2021-10-16 18:40:34 +0900

    refactor: Refactor Form Validation JS

--- 13_formValidation/js/auth.js
+++ 13_formValidation/js/auth.js
@@ -1,65 +1,16 @@
 import toaster from './toaster.js';
+import {
+  INPUT_TYPE_INFO,
+  isValidate,
+  initializeValidateStates,
+  isEqualPw,
+  isValidateAll
+} from './states.js';
 
 // DOM Nodes
 const $signinForm = document.querySelector('.form.signin');
 const $signupForm = document.querySelector('.form.signup');
 const $links = document.querySelectorAll('.link > a');
-
-// states
-const INPUT_TYPE_INFO = {
-  userid: {
-    message: '이메일 형식에 맞게 입력해 주세요.',
-    regExp:
-      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
-    validateState: false
-  },
-  username: {
-    message: '이름을 입력해 주세요.',
-    regExp: /^[A-Za-z가-힣]+$/,
-    validateState: false
-  },
-  password: {
-    message: '영문 또는 숫자를 6~12자 입력해 주세요.',
-    regExp: /^[0-9a-zA-Z]{6,12}$/,
-    validateState: false
-  },
-  'confirm-password': {
-    message: '패스워드가 일치하지 않습니다.',
-    validateState: false
-  }
-};
-
-// functions
-const isValidate = (inputType, inputVal) => {
-  const validation = INPUT_TYPE_INFO[inputType].regExp.test(inputVal);
-  INPUT_TYPE_INFO[inputType].validateState = validation;
-  return validation;
-};
-
-const initializeValidateStates = () => {
-  [...Object.keys(INPUT_TYPE_INFO)].forEach(inputType => {
-    INPUT_TYPE_INFO[inputType].validateState = false;
-  });
-};
-
-const isEqualPw = () => {
-  //   const $signupPassword = document.getElementById('signup-password');
-  //   const $signupConfirmPassword = document.getElementById(
-  //     'signup-confirm-password'
-  //   );
-
-  //     INPUT_TYPE_INFO['confirm-password'].validateState =
-  //       $signupPassword.value === $signupConfirmPassword.value;
-
-  const $passwordInputs = $signupForm.querySelectorAll(
-    'input[type = password]'
-  );
-  INPUT_TYPE_INFO['confirm-password'].validateState = [
-    ...$passwordInputs
-  ].every($input => $input.value === $passwordInputs[0].value);
-
-  return INPUT_TYPE_INFO['confirm-password'].validateState;
-};
 
 const printFormData = $form => {
   $form.method = 'POST';
@@ -70,47 +21,46 @@ const printFormData = $form => {
 };
 
 // Event Bindings
-const $inputContainers = document.querySelectorAll('.input-container');
+export default () => {
+  const $inputContainers = document.querySelectorAll('.input-container');
 
-[...$inputContainers].forEach($container => {
-  const $input = $container.querySelector('input');
-  const $iconSuccess = $container.querySelector('.icon-success');
-  const $iconError = $container.querySelector('.icon-error');
-  const $error = $container.querySelector('.error');
-  const $submitBtn = $container.parentNode.querySelector('.button');
+  [...$inputContainers].forEach($container => {
+    const $input = $container.querySelector('input');
+    const $iconSuccess = $container.querySelector('.icon-success');
+    const $iconError = $container.querySelector('.icon-error');
+    const $error = $container.querySelector('.error');
+    const $submitBtn = $container.parentNode.querySelector('.button');
 
-  $input.oninput = () => {
-    const validation =
-      $input.name === 'confirm-password'
-        ? isEqualPw()
-        : isValidate($input.name, $input.value);
+    $input.oninput = () => {
+      const validation =
+        $input.name === 'confirm-password'
+          ? isEqualPw()
+          : isValidate($input.name, $input.value);
 
-    $iconSuccess.classList.toggle('hidden', !validation);
-    $iconError.classList.toggle('hidden', validation);
-    $error.innerHTML = validation ? '' : INPUT_TYPE_INFO[$input.name].message;
-    const isValidateAll = [
-      ...$container.parentNode.querySelectorAll('input')
-    ].every($input => INPUT_TYPE_INFO[$input.name].validateState);
+      $iconSuccess.classList.toggle('hidden', !validation);
+      $iconError.classList.toggle('hidden', validation);
+      $error.innerHTML = validation ? '' : INPUT_TYPE_INFO[$input.name].message;
 
-    isValidateAll
-      ? $submitBtn.removeAttribute('disabled')
-      : $submitBtn.setAttribute('disabled', '');
-  };
-});
+      isValidateAll($container)
+        ? $submitBtn.removeAttribute('disabled')
+        : $submitBtn.setAttribute('disabled', '');
+    };
+  });
 
-[$signinForm, $signupForm].forEach($form => {
-  $form.onsubmit = e => {
-    e.preventDefault();
-    toaster(e);
-    printFormData($form);
-  };
-});
+  [$signinForm, $signupForm].forEach($form => {
+    $form.onsubmit = e => {
+      e.preventDefault();
+      toaster(e);
+      printFormData($form);
+    };
+  });
 
-$links.forEach($el => {
-  $el.onclick = () => {
-    [$signinForm, $signupForm].forEach($form => {
-      $form.classList.toggle('hidden');
-    });
-    initializeValidateStates();
-  };
-});
+  $links.forEach($el => {
+    $el.onclick = () => {
+      [$signinForm, $signupForm].forEach($form => {
+        $form.classList.toggle('hidden');
+      });
+      initializeValidateStates();
+    };
+  });
+};
