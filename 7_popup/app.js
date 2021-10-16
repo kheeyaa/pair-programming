b3c38db5ff773da0dcc08eb80c67a3cd0055a2d8
skojphy <skojphy@gmail.com>
2021-10-16 20:19:27 +0900

    debug: Debug Popup Js
    모달창 바깥 영역을 클릭할 때 모달창이 닫히도록 수정함.

--- 7_popup/app.js
+++ 7_popup/app.js
@@ -1,22 +1,24 @@
+// DOM Nodes
 const $togglePopup = document.querySelector('.toggle-popup');
 const $modalWrap = document.querySelector('.modal-wrap');
-const $modalCancel = document.querySelector('.modal-cancel');
-const $modalClose = document.querySelector('.modal-close');
 const $modalInput = document.querySelector('.modal-input');
 const $modalForm = document.querySelector('.modal-form');
 const $popupMessage = document.querySelector('.popup-message');
 
+// event bindings
 $togglePopup.onclick = () => {
   $modalInput.value = '';
   $modalWrap.classList.remove('hidden');
   $modalInput.focus();
 };
 
-[$modalCancel, $modalClose].forEach($modalBtn => {
-  $modalBtn.onclick = () => {
-    $modalWrap.classList.add('hidden');
-  };
-});
+$modalWrap.onclick = e => {
+  ['modal-wrap', 'modal-cancel', 'bx-x'].forEach(className => {
+    if (e.target.classList.contains(className)) {
+      $modalWrap.classList.add('hidden');
+    }
+  });
+};
 
 $modalForm.onsubmit = e => {
   e.preventDefault();
